b044b5f61cf2c3ac3221980c946ef5a2e80383cf
Dongwook <cpawookie@gmail.com>
2020-06-09 14:00:32 +0900

    Toastify_and_createAccount_Auth

---
+++ src/Components/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { gql } from "apollo-boost";
+import styled, { ThemeProvider } from "styled-components";
+import GlobalStyles from '../Styles/GlobalStyles';
+import { useQuery } from "react-apollo-hooks";
+import { ToastContainer, toast } from "react-toastify";
+import Theme from "../Styles/Theme"
+import Router from "./Router"
+import Footer from "./Footer";
+
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 935px;
+  width: 100%;
+`;
+
+export default () => {
+  const { data : {isLoggedIn} } = useQuery(QUERY);
+  
+  return (
+    <ThemeProvider theme={Theme}>
+      <Wrapper>
+        <GlobalStyles />
+        <Router isLoggedIn={isLoggedIn} />
+        <Footer />
+        <ToastContainer position={toast.POSITION.BOTTOM_RIGHT} />
+      </Wrapper>
+    </ThemeProvider>
+  );
+};
