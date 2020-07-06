13bb7f98fae021dc919b56d7efb3393dca5da69c
Dongwook <cpawookie@gmail.com>
2020-07-06 14:50:08 +0900

    5.8 Autentication & Log in
    completed Auth , log in

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import styled, { ThemeProvider } from "styled-components";
-import { HashRouter as Router } from "react-router-dom";
+import GlobalStyles from "../Styles/GlobalStyles";
 import { useQuery } from "react-apollo-hooks";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import GlobalStyles from "../Styles/GlobalStyles";
+import Router from "./Router";
 import Theme from "../Styles/Theme";
-import Routes from "./Routes";
 import Footer from "./Footer";
-import Header from "./Header";
 
 const QUERY = gql`
   {
@@ -19,7 +17,7 @@ const QUERY = gql`
 
 const Wrapper = styled.div`
   margin: 0 auto;
-  max-width: ${props => props.theme.maxWidth};
+  max-width: 935px;
   width: 100%;
 `;
 
@@ -30,19 +28,12 @@ export default () => {
 
   return (
     <ThemeProvider theme={Theme}>
-      <>
+      <Wrapper>
         <GlobalStyles />
-        <Router>
-          <>
-            {isLoggedIn && <Header />}
-            <Wrapper>
-              <Routes isLoggedIn={isLoggedIn} />
-              <Footer />
-            </ Wrapper>
-          </>
-        </Router> 
+        <Router isLoggedIn={isLoggedIn} />
+        <Footer />
         <ToastContainer position={toast.POSITION.BOTTOM_RIGHT} />
-      </>
+      </Wrapper>
     </ThemeProvider>
   );
 };
