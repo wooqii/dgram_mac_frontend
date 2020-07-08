55fd22014ed59965bcddf273ed0329885f38a199
Dongwook <cpawookie@gmail.com>
2020-07-08 16:09:44 +0900

    FAQ

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -1,5 +1,5 @@
 import ApolloClient from "apollo-boost";
-import { defaults, resolvers } from "./LocalState";
+import { defaults, resolvers } from "./LocalState"
 
 export default new ApolloClient({
     uri: "http://localhost:4000",
